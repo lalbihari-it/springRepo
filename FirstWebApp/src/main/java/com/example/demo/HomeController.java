b9a8438ebb0487bf93856b29b713e4ea562ceed4
LalBihari <LalBihari@9.199.33.120>
2020-02-11 16:51:43 +0530

    annotation changes

--- FirstWebApp/src/main/java/com/example/demo/HomeController.java
+++ FirstWebApp/src/main/java/com/example/demo/HomeController.java
@@ -2,14 +2,46 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
-	@RequestMapping("home")
-	public String home() {
-		System.out.println("i am home method");
-		return "home.jsp";
-	}
+	/*
+	 * @RequestMapping("home") public String home() {
+	 * System.out.println("i am home method"); return "home.jsp"; }
+	 * 
+	 */
+	
+	//Spring is calling jsp by RequestDispatcher method not sendRedirect
+	/*
+	 * @RequestMapping("home") public String home(HttpServletRequest req) { String
+	 * name = req.getParameter("name");
+	 * 
+	 * req.setAttribute("name", name); //HttpSession session = req.getSession();
+	 * //session.setAttribute("name", name);
+	 * 
+	 * System.out.println("My name is: " + name); return "home"; }
+	 */
 
+	@RequestMapping("home")
+	//public String home(String name, HttpSession session) {
+	//public String home(@RequestParam("name") String name, HttpSession session) {
+	/*public ModelAndView home(@RequestParam("name") String name) {
+		//System.out.println("My name is: " + name);
+		//session.setAttribute("name", name);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", name);
+		mv.setViewName("home");
+		
+		return mv;
+	}*/
+	
+	public ModelAndView student(Student std) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("obj", std);
+		mv.setViewName("home1");
+		return mv;
+	}
 }
--- FirstWebApp/src/main/webapp/pages/home.jsp
+++ FirstWebApp/src/main/webapp/pages/home.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+    pageEncoding="ISO-8859-1" isErrorPage="false"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
- Hi, i am home.jsp
+ Welcome ${name}
+ 
 </body>
 </html>
